863d9682e67eb84a7871b3935269d0c5da9c8ac7
Ron Woods <ron.woods@dreamworks.com>
2024-03-07 10:41:26 -0800

    MOONRAY-5137, MOONRAY-5212: remove scons build files.. (#141)

--- package.py
+++ package.py
@@ -23,13 +23,6 @@ authors = ['Dreamworks Animation R&D - JoSE Team',
 help = ('For assistance, '
         "please contact the folio's owner at: psw-jose@dreamworks.com")
 
-if 'scons' in sys.argv:
-    build_system = 'scons'
-    build_system_pbr = 'bart_scons-10'
-else:
-    build_system = 'cmake'
-    build_system_pbr = 'cmake_modules'
-
 variants = [
     ['os-CentOS-7', 'refplat-vfx2021.0'],
     ['os-CentOS-7', 'refplat-vfx2022.0'],
@@ -37,16 +30,12 @@ variants = [
     ['os-rocky-9', 'refplat-vfx2022.0'],
 ]
 
-sconsTargets = {}
-for variant in variants:
-    sconsTargets[variant[0]] = ['@install']
-
 requires = [
     'arras4_core-4.10'
 ]
 
 private_build_requires = [
-    build_system_pbr,
+    'cmake_modules',
     'gcc-9.3.x'
 ]
 
